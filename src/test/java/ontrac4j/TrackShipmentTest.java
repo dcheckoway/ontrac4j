42030d8b90d21112d99c8b20c97ea69744143392
Dan Checkoway <dcheckoway@gmail.com>
2014-06-10 11:19:15 -0400

    updated unit test for tracking

--- src/test/java/ontrac4j/TrackShipmentTest.java
+++ src/test/java/ontrac4j/TrackShipmentTest.java
@@ -23,6 +23,11 @@ public class TrackShipmentTest extends BaseTest {
         assertNotNull(trackingShipment);
         assertTrue(trackingShipment.isDelivered());
         assertEquals("ADAM ZBAR", trackingShipment.getName());
+        if (trackingShipment.isDelivered()) {
+            System.out.println("Shipment " + trackingShipment.getTracking() + " was shipped on " + trackingShipment.getShipDate() + " and delivered to " + trackingShipment.getTracking() + " on " + trackingShipment.getExpDelDate());
+        } else {
+            System.out.println("Shipment " + trackingShipment.getTracking() + " was shipped on " + trackingShipment.getShipDate() + " and is expected to be delivered to " + trackingShipment.getName() + " on " + trackingShipment.getExpDelDate());
+        }
 
         for (Event event : trackingShipment.getEvents().getEvents()) {
             System.out.println("EVENT: status=" + event.getStatus() + ", description=" + event.getDescription() + ", eventTime=" + event.getEventTime() + ", facility=" + event.getFacility() + ", city=" + event.getCity() + ", state=" + event.getState());
